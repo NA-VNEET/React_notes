7a585237da799561d0d7ebf1308d4ddb048aea70
Navneet <nsh12727@gmail.com>
2024-10-05 21:59:20 +0530

    created custom react

--- 01vite_React/src/main.jsx
+++ 01vite_React/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const anothertElement = (
+  <a href="https://google.com" target="_blank">Visit google</a>
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <anothertElement></anothertElement>
   </StrictMode>,
 )
